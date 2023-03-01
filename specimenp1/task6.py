1bce7eca236e2b29d248d21ab7f042c76a421225
bobsobek <125616891+bobsobek@users.noreply.github.com>
2023-03-01 10:39:15 +0800

    Update task6.py

--- specimenp1/task6.py
+++ specimenp1/task6.py
@@ -3,11 +3,11 @@ for i in range (5):
     h = input()
     ppl.append(h)
 a = 0
-while a < 4:
+while a < 5:
     if ppl[a] == 'Ellie' :
         ans = a 
     a = a + 1
     
 place = ['1st','2nd','3rd','4th','5th']
-ans = place [a]
+ans = place [ans]
 print(ans)
